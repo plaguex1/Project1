4cbe27d515bc8f1584677a625bc4e65500bdac1e
plaguex1 <plaguex1@gmail.com>
2017-02-06 16:44:33 -0500

    add servlet

--- src/java/project1/business/ProjectServlet.java
+++ src/java/project1/business/ProjectServlet.java
@@ -8,9 +8,11 @@ package project1.business;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
